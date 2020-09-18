f980cf2d308d8937e9af2820b4095e0d1857f69f
Johan Sundkvist <johansundqvist96@gmail.com>
2020-09-18 15:05:07 +0200

    removed grpc-out from protoc

--- third_party/protoc-gen.sh
+++ third_party/protoc-gen.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-protoc --proto_path=api/proto/v1 --proto_path=third_party --go-grpc_out=pkg/generated/v1 --go_out=plugins=grpc:pkg/generated/v1 ns.proto
+protoc --proto_path=api/proto/v1 --proto_path=third_party --go_out=plugins=grpc:pkg/generated/v1 ns.proto

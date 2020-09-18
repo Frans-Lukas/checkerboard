db11e0a7d299573baca07d1ea1a83c2308266e85
Johan Sundkvist <johansundqvist96@gmail.com>
2020-09-18 14:50:27 +0200

    started work on cellmanager

--- pkg/created/v1/nameserver.go
+++ pkg/created/v1/nameserver.go
@@ -1,1 +1,12 @@
 package v1
+
+import (
+	"context"
+	pb "github.com/Frans-Lukas/checkerboard/pkg/generated/v1"
+)
+
+type CellManager struct {
+	pb.CellManagerServer
+}
+
+func (s *CellManager) CreateCell (ctx context.Context)

6ddcecf7090624de435d83718ef82ae156aba12e
HissPirat <franslukas@gmail.com>
2020-09-19 12:11:04 +0200

    cleanup

--- testing/created/v1/cellmanager_test.go
+++ testing/created/v1/cellmanager_test.go
@@ -14,7 +14,6 @@ func TestCreateCell(t *testing.T) {
 	_, err := cm.CreateCell(context.Background(), &request)
 	failIf(err, "could not create cell")
 	if (*cm.Cells)[0].CellId != "testId" {
-		err = errors.New("cell was not inserted")
-		fatalFail(err)
+		fatalFail(errors.New("cell was not inserted"))
 	}
 }

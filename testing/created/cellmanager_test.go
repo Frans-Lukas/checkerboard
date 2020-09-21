89db39a03721304a52b17ae70fa73fc0b91a668e
HissPirat <franslukas@gmail.com>
2020-09-21 11:11:18 +0200

    list players works

--- testing/created/cellmanager_test.go
+++ testing/created/cellmanager_test.go
@@ -19,12 +19,22 @@ func TestCreateCell(t *testing.T) {
 	}
 }
 
+func TestCreateCellCreatesEmptyPlayerList(t *testing.T) {
+	cm := cellmanager.NewCellManager()
+	request := generated.CellRequest{CellId: "testId"}
+	_, err := cm.CreateCell(context.Background(), &request)
+	failIfNotNull(err, "could not create cell")
+	if (*cm.Cells)[0].Players == nil {
+		fatalFail(errors.New("cell did not create empty player list with CreateCell"))
+	}
+}
+
 func TestListCells(t *testing.T) {
 	cm := cellmanager.NewCellManager()
 	cm.AppendCell(cell.Cell{CellId: "testId1"})
 	cm.AppendCell(cell.Cell{CellId: "testId2"})
 	cellList, err := cm.ListCells(context.Background(), &generated.ListCellsRequest{})
-	failIfNotNull(err, "could not create cell")
+	failIfNotNull(err, "could not list cells")
 	testId1Exists := false
 	testId2Exists := false
 	for _, cellId := range cellList.CellId {
@@ -38,4 +48,22 @@ func TestListCells(t *testing.T) {
 	if !testId1Exists || !testId2Exists {
 		fatalFail(errors.New("cells were not returned from ListCells"))
 	}
+}
+
+func TestListPlayersInCell(t *testing.T) {
+	cm := cellmanager.NewCellManager()
+	cm.AppendCell(cell.Cell{CellId: "testId1"})
+	testIp := "192.168.16.1"
+	(*cm.Cells)[0].AppendPlayer(cell.Player{Ip: testIp, Port: 1337})
+	playerList, err := cm.ListPlayersInCell(
+		context.Background(), &generated.ListPlayersRequest{CellId: "testId1"},
+	)
+	failIfNotNull(err, "could not list players in cell")
+	if len(playerList.Port) == 0 || len(playerList.Ip) == 0 {
+		fatalFail(errors.New("players were not returned from ListPlayersInCell"))
+	}
+	if playerList.Ip[0] == testIp && playerList.Port[0] == 1337 {
+		return
+	}
+	fatalFail(errors.New("incorrect players were returned from ListPlayersInCell"))
 }

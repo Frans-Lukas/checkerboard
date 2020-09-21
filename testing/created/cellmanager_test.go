99e4dc70f58194e1d4dde1471adbdf273957d397
HissPirat <franslukas@gmail.com>
2020-09-21 10:45:26 +0200

    added ListCells test

--- testing/created/cellmanager_test.go
+++ testing/created/cellmanager_test.go
@@ -3,6 +3,7 @@ package created
 import (
 	"context"
 	"errors"
+	"github.com/Frans-Lukas/checkerboard/pkg/created/cell"
 	"github.com/Frans-Lukas/checkerboard/pkg/created/cellmanager"
 	"github.com/Frans-Lukas/checkerboard/pkg/generated"
 	"testing"
@@ -14,6 +15,27 @@ func TestCreateCell(t *testing.T) {
 	_, err := cm.CreateCell(context.Background(), &request)
 	failIfNotNull(err, "could not create cell")
 	if (*cm.Cells)[0].CellId != "testId" {
-		fatalFail(errors.New("cell was not inserted"))
+		fatalFail(errors.New("cell was not inserted with CreateCell"))
+	}
+}
+
+func TestListCells(t *testing.T) {
+	cm := cellmanager.NewCellManager()
+	cm.AppendCell(cell.Cell{CellId: "testId1"})
+	cm.AppendCell(cell.Cell{CellId: "testId2"})
+	cellList, err := cm.ListCells(context.Background(), &generated.ListCellsRequest{})
+	failIfNotNull(err, "could not create cell")
+	testId1Exists := false
+	testId2Exists := false
+	for _, cellId := range cellList.CellId {
+		if cellId == "testId1" {
+			testId1Exists = true
+		}
+		if cellId == "testId2" {
+			testId2Exists = true
+		}
+	}
+	if !testId1Exists || !testId2Exists {
+		fatalFail(errors.New("cells were not returned from ListCells"))
 	}
 }

ddff2b491f0b70a360d4bcf9dff71ca2f3f96ec9
Frans-lukas Lövenvald <c16fld@cs.umu.se>
2020-09-29 13:34:09 +0200

    changed proto files

--- pkg/created/cell/objects/cellmaster.go
+++ pkg/created/cell/objects/cellmaster.go
@@ -10,10 +10,6 @@ type CellMaster struct {
 	ObjectsToUpdate *[]objects.SingleObject
 }
 
-func (cm *CellMaster) SendUpdate(ctx context.Context, in *objects.SingleObject, ) (*objects.EmptyReply, error) {
-	cm.AppendObjectToUpdate(*in)
-	return &objects.EmptyReply{}, nil
-}
 
 func NewCellMaster() CellMaster {
 	emptyList := make([]objects.SingleObject, 0)
@@ -23,3 +19,21 @@ func NewCellMaster() CellMaster {
 func (cm *CellMaster) AppendObjectToUpdate(object objects.SingleObject) {
 	*cm.ObjectsToUpdate = append(*cm.ObjectsToUpdate, object)
 }
+
+func (cm *CellMaster) SendUpdate(ctx context.Context, in *objects.SingleObject, ) (*objects.EmptyReply, error) {
+	cm.AppendObjectToUpdate(*in)
+	return &objects.EmptyReply{}, nil
+}
+
+
+
+func (cm *CellMaster) RequestMutatingObjects(ctx context.Context, in *objects.EmptyRequest) (*objects.MultipleObjects, error) {
+	//cm.AppendObjectToUpdate(*in)
+	return &objects.MultipleObjects{}, nil
+}
+
+
+//rpc RequestMutatingObjects (MultipleObjects) returns (MultipleObjects) {}
+
+
+
